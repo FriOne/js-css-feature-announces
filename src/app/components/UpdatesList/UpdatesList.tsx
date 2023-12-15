9abf7c2260a8f98612bf28b91e44ca8dd47eed1d
Lyubimov Roman <lubimov.nsk@gmail.com>
2023-12-15 14:53:12 +0700

    styles: make styles simir to caniuse

--- src/app/components/UpdatesList/UpdatesList.tsx
+++ src/app/components/UpdatesList/UpdatesList.tsx
@@ -1,14 +1,38 @@
 'use client';
+import { useMemo } from 'react';
 
 import { trpc } from '@/app/utils/trpc';
 import { useIntersectionObserver } from '@/app/hooks/useIntersectionObserver';
 import { useUpdates } from '@/app/components/UpdatesList/useUpdates';
 import { getFeatureLinkByKey } from '@/app/utils/getFeatureLinkByKey';
+import { FeatureUpdateSchemaType } from '@/server/schemas/featureUpdateSchema';
 
 import styles from './UpdatesList.module.css';
 
+type FeatureUpdateWithStringDates = Omit<FeatureUpdateSchemaType, 'createdAt' | 'updatedAt'> & {
+  createdAt: string;
+  updatedAt: string;
+};
+type UpdatesByDate = Record<string, FeatureUpdateWithStringDates[]>;
+
 export const UpdatesList = trpc.withTRPC(() => {
   const { isLoading, isFetching, data, error, hasNextPage, fetchNextPage } = useUpdates();
+  const updatesByDate = useMemo(() => {
+    if (!data) {
+      return;
+    }
+
+    return data.pages.reduce((acc, nextPage) => {
+      for (const item of nextPage.items) {
+        const date = item.updatedAt.slice(0, 10);
+        acc[date] = acc[date] || [];
+        acc[date].push(item);
+      }
+
+      return acc;
+    }, {} as UpdatesByDate);
+  }, [data]);
+
   const observerTargetRef = useIntersectionObserver(() => {
     if (!isFetching) {
       fetchNextPage();
@@ -16,27 +40,30 @@ export const UpdatesList = trpc.withTRPC(() => {
   }, [data, isFetching]);
 
   const renderContent = () => {
-    if (isLoading && !data) {
-      return 'loading...';
+    if (isLoading || !data) {
+      return <div className={styles.loading}>loading</div>;
     }
 
     if (error) {
-      return `Error: ${error.message}`;
+      return <div className={styles.error}>{`Error: ${error.message}`}</div>;
     }
 
     return (
       <>
-        {data.pages.map(({ items }, index) => (
-          <ul key={index} className={styles.page}>
-            {items.map((item) => (
-              <li key={item.key} className={styles.update}>
-                <a className={styles.title} href={getFeatureLinkByKey(item.key)}>
-                  {item.title}
-                </a>
-                <div className={styles.description}>{item.description}</div>
-              </li>
-            ))}
-          </ul>
+        {Object.entries(updatesByDate).map(([date, items], index) => (
+          <section key={date} className={styles.itemsByDate}>
+            <h2 className={styles.date}>{date}</h2>
+            <ul key={index} className={styles.itemsList}>
+              {items.map((item) => (
+                <li key={item.key} className={styles.update}>
+                  <a className={styles.title} href={getFeatureLinkByKey(item.key)} target="_blank">
+                    {item.title}
+                  </a>
+                  <div className={styles.description}>{item.description}</div>
+                </li>
+              ))}
+            </ul>
+          </section>
         ))}
         {hasNextPage && <div className={styles.observer} ref={observerTargetRef} />}
         {hasNextPage && <div className={styles.nextLoading}>Loading...</div>}
