7360e38c8d86a71741e559d0c79ff3102cdde2e5
Lyubimov Roman <lubimov.nsk@gmail.com>
2023-12-18 19:41:33 +0700

    feat: adds link to github repo and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,28 @@ import styles from './page.module.css';
 
 export default async function Home() {
   return (
-    <main className={styles.main}>
-      <UpdatesList />
-    </main>
+    <>
+      <a
+        className={styles.githubLink}
+        target="_blank"
+        aria-label="Go to the GitHub repository"
+        href="https://github.com/FriOne/js-css-feature-announces"
+      >
+        <span></span>
+      </a>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>I Can Use</h1>
+        <div className={styles.description}>
+          The project notifies about JS/CSS features that cross 3 percent global usage. You can
+          subscribe to the{' '}
+          <a target="_blank" href="https://t.me/js_css_updates_3">
+            telegram chat
+          </a>{' '}
+          to receive these updates.
+        </div>
+        <UpdatesList />
+      </main>
+    </>
   );
 }
